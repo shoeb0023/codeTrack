cb2edf080075265f1f8c9721d5b7122122a3e44f
shoeb0023 <142240707+shoeb0023@users.noreply.github.com>
2026-09-15 21:05:48 +0530

    Solve [LeetCode] 2: Add Two Numbers

--- LeetCode/0002_Add_Two_Numbers.cpp
+++ LeetCode/0002_Add_Two_Numbers.cpp
@@ -1,16 +1,16 @@
-
-            if (l2 != nullptr) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-
-            curr->next = new ListNode(sum % 10);
-
-            carry = sum / 10;
-
-            curr = curr->next;
-        }
-
-        return dummy->next;
-    }
-};
+        carry != 0:
+            total_sum = carry
+            
+            if l1 is not None:
+                total_sum += l1.val
+                l1 = l1.next
+                
+            if l2 is not None:
+                total_sum += l2.val
+                l2 = l2.next
+                
+            curr.next = ListNode(total_sum % 10)
+            carry = total_sum // 10
+            curr = curr.next
+            
+        return dummy.next
