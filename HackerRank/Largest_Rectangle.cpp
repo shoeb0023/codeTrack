f62b42a4165f55198392c4ffad9b385c9fdfe919
shoeb0023 <142240707+shoeb0023@users.noreply.github.com>
2026-09-16 01:44:17 +0530

    Solve [HackerRank] : Largest Rectangle

--- HackerRank/Largest_Rectangle.cpp
+++ HackerRank/Largest_Rectangle.cpp
@@ -1,35 +1,9 @@
-    );
+- start));
 
-    return s;
-}
+        start = end + 1;
+    }
 
-string rtrim(const string &str) {
-    string s(str);
+    tokens.push_back(str.substr(start));
 
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1
-        (ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != 
-    string::npos) {
-        tokens.push_back(str.substr(start, 
-        end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
+    return tokens;
 }
