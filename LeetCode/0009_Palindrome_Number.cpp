3f3abe9ab204ada2bca5ce4c148c26e53d10fd5b
shoeb0023 <142240707+shoeb0023@users.noreply.github.com>
2026-09-16 01:42:50 +0530

    Solve [LeetCode] 9: Palindrome Number

--- LeetCode/0009_Palindrome_Number.cpp
+++ LeetCode/0009_Palindrome_Number.cpp
@@ -1,13 +1,17 @@
-
-        // Reverse only half of the number
-        while (x > reverse) {
-            int digit = x % 10;
-            reverse = reverse * 10 + digit;
-            x = x / 10;
-        }
-
-        // Even digits: x == reverse
-        // Odd digits: x == reverse / 10
-        return x == reverse || x == reverse / 10;
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    // Negative numbers and numbers that end with 0 
+    (except 0 itself) cannot be palindromes
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+        return false;
     }
-};
+
+    let revertedNumber = 0;
+    while (x > revertedNumber) {
+        revertedNumber = revertedNumber * 10 + (x % 10);
+        x = Math.trunc(x / 10);
+    }
+
