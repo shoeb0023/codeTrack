aafe66c7789e01e28a10c1679f0b67d09fb5c1f0
shoeb0023 <142240707+shoeb0023@users.noreply.github.com>
2026-09-16 01:24:13 +0530

    Solve [HackerRank] : Largest Rectangle

--- HackerRank/Largest_Rectangle.cpp
+++ HackerRank/Largest_Rectangle.cpp
@@ -1,37 +1,35 @@
+    );
 
-            int height = h[st.top()];
-            st.pop();
-
-            int width;
-
-            if (st.empty()) {
-                width = i;
-            } else {
-                width = i - st.top() - 1;
-            }
-
-            long long area = 1LL * height * 
-            width;
-
-            maxArea = max(maxArea, area);
-        }
-
-        st.push(i);
-    }
-
-    return maxArea;
+    return s;
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
+string rtrim(const string &str) {
+    string s(str);
 
-    string n_temp;
-    getline(cin, n_temp);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1
+        (ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
 
-    int n = stoi(ltrim(rtrim(n_temp)));
+    return s;
+}
 
-    string h_temp_temp;
-    getline(cin, h_temp_temp);
+vector<string> split(const string &str) {
+    vector<string> tokens;
 
-    vector<string> h_temp = split(rtrim
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != 
+    string::npos) {
+        tokens.push_back(str.substr(start, 
+        end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
