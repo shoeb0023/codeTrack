02585298f8ed5f76ed100ec165804deb7c940aea
shoeb0023 <142240707+shoeb0023@users.noreply.github.com>
2026-09-15 21:06:11 +0530

    Solve [LeetCode] 2: Add Two Numbers

---
+++ LeetCode/0002_Add_Two_Numbers.py
@@ -0,0 +1,17 @@
+        carry != 0:
+            total_sum = carry
+            
+        while l1 is not None or l2 is not None or 
+        
+        carry = 0
+        curr = dummy
+        dummy = ListNode(0)
+    l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(self, l1: Optional[ListNode], 
+class Solution:
+
+#         self.next = next
+#         self.val = val
+#     def __init__(self, val=0, next=None):
+# class ListNode:
+# Definition for singly-linked list.
