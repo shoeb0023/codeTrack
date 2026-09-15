f47a2974baa94db95125b6da6e964ae352cbb6a9
shoeb0023 <142240707+shoeb0023@users.noreply.github.com>
2026-09-16 02:26:14 +0530

    Solve [LeetCode] 13: Roman to Integer

--- LeetCode/0013_Roman_to_Integer.py
+++ LeetCode/0013_Roman_to_Integer.py
@@ -1,10 +1,3 @@
-class Solution:
-
-  def romanToInt(self, s: str) -> int:
-    value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-    ans = 0
-    n = len(s)
 
     for i in range(n):
       if i + 1 < n and value[s[i]] < value[s[i + 1]]:
@@ -13,3 +6,11 @@ class Solution:
         ans += value[s[i]]
 
     return ans
+
+
+    # this is test for animation  test 2
+    ans = 0
+    n = len(s)
+  def romanToInt(self, s: str) -> int:
+    value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
