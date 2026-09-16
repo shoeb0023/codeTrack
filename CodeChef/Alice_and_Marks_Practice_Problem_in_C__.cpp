9c9fd320b1c7188f3e419c30fb20ced027ae5e3e
shoeb0023 <142240707+shoeb0023@users.noreply.github.com>
2026-09-16 18:37:55 +0530

    Solve [CodeChef] Alice and Marks Practice Problem in C++

---
+++ CodeChef/Alice_and_Marks_Practice_Problem_in_C__.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    if (X >= 2 * Y)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
